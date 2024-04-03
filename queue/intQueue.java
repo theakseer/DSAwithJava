5d643e450380b60c3ea7ea69e575d309b682a4bb
[Akseer Ahmad] <68474681+theakseer@users.noreply.github.com>
2024-04-03 15:58:18 +0530

    added intqueue and interface queue

---
+++ queue/intQueue.java
@@ -0,0 +1,21 @@
+package queue;
+
+public class intQueue implements Queue<Integer>{
+    
+    public void enqueue(int element){
+        
+    };
+    public int dequeue(int element){
+        return 1;
+    };
+    public int peek(int element){
+        return 1;
+    };
+    public int size(){
+        return queue.size();
+    };
+    public boolean isEmpty(){
+        return size()==0;
+    };
+
+}
